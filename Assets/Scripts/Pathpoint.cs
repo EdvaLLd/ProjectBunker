870f6d9bd4236b31fdb46681f271e0e042d598e4
spooderMan420 <spindelman2000@gmail.com>
2023-11-12 22:10:30 +0100

    Alexander_0.1.3
    + Unit can be selected (sort of, just symbolical right now).
    + Unit will now pathfind and move along that path.
    + Pathpoints can now be selected.
    + Upon selection unit and pathpoint will change colour.
    + Upon unit arrival at final pathpoint in path, pathpoint colour will revert to nonselected.
    + Unit now uses 2DCollider.
    * Very buggy, easy to break.

--- Assets/Scripts/Pathpoint.cs
+++ Assets/Scripts/Pathpoint.cs
@@ -15,6 +15,16 @@ public class Pathpoint : MonoBehaviour
     public float total = 0;
     [HideInInspector]
     public Pathpoint discoveryPoint = null;
+    
+    [HideInInspector]
+    public bool mouseIsHovering = false;
+    /*[HideInInspector]
+    public bool allowSelection = true;*/
+    //[HideInInspector]
+    public bool isSelected = false;
+
+    [SerializeField]
+    private Material[] selectionStateMaterials = new Material[2];
 
     public void ResetPoint()
     {
@@ -22,5 +32,59 @@ public class Pathpoint : MonoBehaviour
         estDistToEnd = 0;
         total = 0;
         discoveryPoint = null;
+    }
+
+    void OnMouseOver()
+    {
+        if (!mouseIsHovering)
+        {
+            //print(gameObject.name + " can be selected.");
+            mouseIsHovering = true;
+        }
+
+            SelectUnit(mouseIsHovering/*, allowSelection*/);
+    }
+
+    void OnMouseExit()
+    {
+        if (mouseIsHovering)
+        {
+            //print(gameObject.name + " can no longer be selected.");
+            mouseIsHovering = false;
+        }
+    }
+
+    private void SelectUnit(bool mouseAbove/*, bool canSelect*/)
+    {
+        /*if (canSelect) 
+        {*/
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (mouseAbove && !isSelected)
+                {
+                    isSelected = true;
+                    print(gameObject.name + " was selected as end.");
+                }
+                else if (mouseAbove && isSelected)
+                {
+                    isSelected = false;
+                    //print(gameObject.name + " was deselected.");
+                }
+                SwapMaterial();
+            }
+        
+        //}
+    }
+
+    public void SwapMaterial() //temporary UI substitute to signify selected. 
+    {
+        if (isSelected)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = selectionStateMaterials[1];
+        }
+        else
+        {
+                gameObject.GetComponent<MeshRenderer>().material = selectionStateMaterials[0];
+        }
     }
 }
--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -7,43 +7,50 @@ using UnityEngine;
 public class UnitController : MonoBehaviour
 {
     [HideInInspector]
-    public bool selectionAllowed = false;
+    public bool mouseIsHovering = false;
     [HideInInspector]
     public bool isSelected = false;
 
     [SerializeField]
-    private Material[] testMaterials = new Material[2];
+    private Material[] selectionStateMaterials = new Material[2];
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float movementSpeed = 1;
+
+    private Vector2 startPoint;
+    private Vector2 immediateDestination;
+    private Vector2 direction;
+
+    private int index = 0;
+
+    private void Update()
     {
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
+        Move();
     }
 
     void OnMouseOver()
     {
-        if (!selectionAllowed)
+        if (!mouseIsHovering)
         {
            //print(gameObject.name + " can be selected.");
-            selectionAllowed = true;
+            mouseIsHovering = true;
         }
 
-        SelectUnit(selectionAllowed);
+        SelectUnit(mouseIsHovering);
     }
 
     void OnMouseExit()
     {
-        if (selectionAllowed)
+        if (mouseIsHovering)
         {
             //print(gameObject.name + " can no longer be selected.");
-            selectionAllowed = false;
+            mouseIsHovering = false;
         }
+    }
+    private Vector2 GetUnitPosition()
+    {
+        //if (!isSelected) {return new Vector3(0,0,0);}
+        return gameObject.transform.position;
     }
 
     private void SelectUnit(bool canSelect)
@@ -53,6 +60,7 @@ public class UnitController : MonoBehaviour
             if (canSelect && !isSelected)
             {
                 isSelected = true;
+                //startPoint = GetUnitPosition();
                 //print(gameObject.name + " was selected.");
             }
             else if (canSelect && isSelected)
@@ -68,11 +76,55 @@ public class UnitController : MonoBehaviour
     {
         if (isSelected) 
         {
-            gameObject.GetComponent<MeshRenderer>().material = testMaterials[1];
+            gameObject.GetComponent<MeshRenderer>().material = selectionStateMaterials[1];
         }
         else
         {
-            gameObject.GetComponent<MeshRenderer>().material = testMaterials[0];
+            gameObject.GetComponent<MeshRenderer>().material = selectionStateMaterials[0];
         }
+    }
+
+    private List<Pathpoint> GetPath(Vector2 start, Vector2 destination) 
+    {
+        return gameObject.GetComponent<Pathfinding>().FindPath(start, destination);
+    }
+
+    private void Move() 
+    {
+        Vector2 finalDestination = startPoint;
+        direction = (/*finalDestination*/immediateDestination - startPoint).normalized;
+
+        Pathpoint[] pointArray = FindObjectsOfType(typeof(Pathpoint)) as Pathpoint[];
+        List<Pathpoint> path;//this is where it is ok, when you undo and this dissapears, you should not undo anymore.
+
+        foreach (Pathpoint point in pointArray) 
+        {
+            if (point.isSelected)
+            {
+                finalDestination = point.transform.position;
+                path = GetPath(startPoint, finalDestination);
+                immediateDestination = path[index].transform.position;
+
+                if (gameObject.GetComponent<Collider2D>().OverlapPoint(immediateDestination) && index < path.Count)
+                {
+                    startPoint = GetUnitPosition();
+                    //point.isSelected = false;
+                    //point.GetComponent<Pathpoint>().SwapMaterial();
+                    index++;
+                }
+                if (gameObject.GetComponent<Collider2D>().OverlapPoint(finalDestination))
+                {
+                    startPoint = finalDestination;
+                    point.isSelected = false;
+                    point.GetComponent<Pathpoint>().SwapMaterial();
+                    index = 0;
+                }
+                print("index: " + index+ ", maxIndex: " + path.Count);
+            }
+        }
+       
+         
+
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
     }
 }
