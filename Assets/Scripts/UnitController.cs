e694c4471da892853394f9e4e9b0503554e23a06
EdvaLLd <63045045+EdvaLLd@users.noreply.github.com>
2023-11-22 16:22:57 +0100

    pathfinding fix och lite ui-saker

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -36,6 +36,30 @@ public class UnitController : MonoBehaviour
     static Character selectedCharacter = null;
 
 
+    //(denna är nog anpassad för procent, så viktigt att variablerna går mellan 0 och 100)
+    //avgör hur ofta UIn uppdateras, 5 = var femte procent
+    int howOftenToUpdateStats = 5;
+
+
+    static GameObject characterStatsWindowStatic;
+
+
+    private void Start()
+    {
+        unSelectedModifier = unSelectedModifierSetter;
+        selectedModifier = selectedModifierSetter;
+        movementSpeed = movementSpeedSetter;
+        Character.onTaskCompletion += TaskCompleted;
+        //Character.onCharacterStatsChange += UpdateCharacterStatsUI;
+
+        uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+
+
+
+        characterStatsWindowStatic = GameObject.FindGameObjectWithTag("CharacterStatsWindow");
+        characterStatsWindowStatic.SetActive(false);
+    }
+
 
     private void Update()
     {
@@ -57,16 +81,27 @@ public class UnitController : MonoBehaviour
         if (selectedCharacter != null)
         {
             UpdateCharacterStatsUI();
+            if(Input.GetMouseButtonDown(1))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float pos;
+                Plane plane = new Plane(Vector3.forward, -Pathfinding.zMoveValue);
+                if(plane.Raycast(ray, out pos))
+                {
+                    selectedCharacter.MoveToPos(ray.GetPoint(pos));
+                }
+                else
+                {
+                    print("något fucky med matten bakom vänster-klicks-movement");
+                }
+            }
         }
     }
 
-    [SerializeField]
-    GameObject characterStatsWindow;
-    static GameObject characterStatsWindowStatic;
     void UpdateCharacterStatsUI()
     {
-        characterStatsWindowStatic.GetComponent<CharacterStatsHandler>().UpdateHealth(selectedCharacter.health);
-        characterStatsWindowStatic.GetComponent<CharacterStatsHandler>().UpdateHunger(selectedCharacter.hunger);
+        characterStatsWindowStatic.GetComponent<CharacterStatsHandler>().UpdateHealth(((int)(selectedCharacter.health / howOftenToUpdateStats) + 1) * howOftenToUpdateStats);
+        characterStatsWindowStatic.GetComponent<CharacterStatsHandler>().UpdateHunger(((int)(selectedCharacter.hunger / howOftenToUpdateStats) + 1) * howOftenToUpdateStats);
     }
 
     public void FeedCharacter(Food food)
@@ -75,23 +110,6 @@ public class UnitController : MonoBehaviour
         {
             selectedCharacter.ConsumeFood(food);
         }
-    }
-
-    private void Start()
-    {
-        unSelectedModifier = unSelectedModifierSetter;
-        selectedModifier = selectedModifierSetter;
-        movementSpeed = movementSpeedSetter;
-        Character.onTaskCompletion += TaskCompleted;
-
-        uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
-
-
-
-        //temp
-        characterStatsWindow.GetComponent<CharacterStatsHandler>().SetUp();
-        characterStatsWindow.SetActive(false);
-        characterStatsWindowStatic = characterStatsWindow;
     }
 
     public void TaskCompleted(Character character)
@@ -112,11 +130,12 @@ public class UnitController : MonoBehaviour
                 TextLog.AddLog($"Inspected item: {character.item.Description}");
                 break;
             case CharacterTasks.looting:
-                ChestContent chest = selectedCharacter.itemInteractedWith.GetComponent<ChestContent>();
+                ChestContent chest = character.itemInteractedWith.GetComponent<ChestContent>();
                 if (chest == null)
                 {
-                    chest = selectedCharacter.itemInteractedWith.gameObject.AddComponent<ChestContent>();
+                    chest = character.itemInteractedWith.gameObject.AddComponent<ChestContent>();
                 }
+                TextLog.AddLog("Interacted with item " + character.itemInteractedWith.name);
                 chest.CheckContent();
                 break;
             default:
@@ -147,11 +166,12 @@ public class UnitController : MonoBehaviour
         else
         {
             if (selectedCharacter != null)
-            {
+            {;
                 setCharacterVisual(selectedCharacter, false);
             }
             selectedCharacter = newSelectedCharacter;
             setCharacterVisual(selectedCharacter, true);
+            characterStatsWindowStatic.GetComponent<CharacterStatsHandler>().SetUp(selectedCharacter);
             characterStatsWindowStatic.SetActive(true);
         }
     }
