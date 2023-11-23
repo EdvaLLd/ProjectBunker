bb50b14e294f156069ad171ae2bf31f3b4a07660
Liam-1234 <104007472+Liam-1234@users.noreply.github.com>
2023-11-23 19:37:55 +0100

    Day and night

---
+++ Assets/Scripts/Skybox/SkyboxController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SkyboxController : MonoBehaviour
+{
+    [SerializeField] Transform _Sun = default;
+    [SerializeField] Transform _Moon = default;
+
+    void LateUpdate()
+    {
+        // Directions are defined to point towards the object
+
+        // Sun
+        Shader.SetGlobalVector("_SunDir", -_Sun.transform.forward);
+
+        // Moon
+        Shader.SetGlobalVector("_MoonDir", -_Moon.transform.forward);
+    }
+}
