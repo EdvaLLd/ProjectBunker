99816ae3a1abaa7f6744bd3dfe76e35fa213ad20
EdvaLLd <63045045+EdvaLLd@users.noreply.github.com>
2023-12-05 20:28:46 +0100

    fixade med hover-fönster

---
+++ Assets/Scripts/ICustomPointerHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICustomPointerHandler
+{
+    void OnPointerEnterCustom();
+    void OnPointerExitCustom();
+}
